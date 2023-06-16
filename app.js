3715e75b89ad4b4016ede098c890541ad03e72d6
sdogukan <115641285+sdogukan@users.noreply.github.com>
2023-06-16 19:39:22 +0300

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ import generateRandomCode from './utils.js'
 import dotenv from 'dotenv'
 dotenv.config()
 
-
 // Fetching environment variables, 
 const { TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN, SENDER_PHONE_NUMBER, RECIPIENT_PHONE_NUMBER } = process.env
 
